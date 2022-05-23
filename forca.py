8d1b98582df398d4aafe4b54be67ec40f1ee5b87
wandersonreiss <wreletronicawr@gmail.com>
2022-05-23 19:35:32 -0300

    adicionando as variaveis e funcoes

--- forca.py
+++ forca.py
@@ -5,3 +5,10 @@ def jogar():
     print("*********************************")
     print("Bem vindo ao jogo de Adivinhação!")
     print("*********************************")
+
+    numero_secreto = random.randrange(1, 101)
+    total_de_tentativas = 0
+    pontos = 1000
+
+    print("Qual nível de dificuldade?")
+    print("(1) Fácil (2) Médio (3) Difícil")
