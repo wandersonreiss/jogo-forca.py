fdba85e107e075dfd5af47582bc5d19a4d916b8f
wandersonreiss <wreletronicawr@gmail.com>
2022-05-23 19:35:32 -0300

    adicionando as variaveis e funcoes

--- forca.py
+++ forca.py
@@ -1,7 +1,34 @@
-import random
 
 def jogar():
 
     print("*********************************")
-    print("Bem vindo ao jogo de Adivinhação!")
+    print("***Bem vindo ao jogo da Forca!***")
     print("*********************************")
+
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
+
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    print(letras_acertadas)
+
+    while (not acertou and not enforcou):
+
+        chute = input("Qual letra? ")
+        chute = chute.strip().upper()
+
+        if (chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+
+        enforcou = erros == 6
+        print(letras_acertadas)
+
+    print("Fim do jogo")
